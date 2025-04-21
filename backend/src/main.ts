73e13607ca7d12fc0f8016c04f4dbad623faed58
oriDEV <ori.nizri.tal@gmail.com>
2025-04-21 09:38:08 +0300

    Nestjs socket.io dependency missing in backend render build fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,6 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-// import { RolesGuard } from './auth/guards/roles.guard';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
@@ -10,7 +8,7 @@ async function bootstrap() {
   const config = app.get(ConfigService);
 
   const isProd = config.get<string>('NODE_ENV') === 'production';
-  // ✅ CORS: Allow dev + production frontend
+  // CORS: Allow dev + production frontend
   app.enableCors({
     origin: [
       'http://localhost:3000', // Development frontend
